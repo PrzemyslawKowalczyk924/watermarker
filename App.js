5bf27075a5fb621336a65d5635d4483c97ee9521
unknown <kowalczyk.przemek@wp.pl>
2021-04-04 02:20:44 +0200

    Add try ...catch to functions image and text marker

--- App.js
+++ App.js
@@ -3,28 +3,36 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
-  const image = await Jimp.read(inputFile);
-  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-  const textData = {
-    text,
-    alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-    alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
-  };
-  image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
-  await image.quality(100).writeAsync(outputFile);
+  try {
+    const image = await Jimp.read(inputFile);
+    const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+    const textData = {
+      text,
+      alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+      alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+    };
+    image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
+    await image.quality(100).writeAsync(outputFile);
+  } catch {
+    console.log('Something went wrong... Try again!');
+  }
 };
 
 const addImageWatermarkToImage = async function(inputFile, outputFile, watermarkFile) {
-  const image = await Jimp.read(inputFile);
-  const watermark = await Jimp.read(watermarkFile);
-  const x = image.getWidth() / 2 - watermark.getWidth() / 2;
-  const y = image.getHeight() / 2 - watermark.getHeight() / 2;
-
-  image.composite(watermark, x, y, {
-    mode: Jimp.BLEND_SOURCE_OVER,
-    opacitySource: 0.5,
-  });
-  await image.quality(100).writeAsync(outputFile);
+  try {
+    const image = await Jimp.read(inputFile);
+    const watermark = await Jimp.read(watermarkFile);
+    const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+    const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+  
+    image.composite(watermark, x, y, {
+      mode: Jimp.BLEND_SOURCE_OVER,
+      opacitySource: 0.5,
+    });
+    await image.quality(100).writeAsync(outputFile);
+  } catch {
+    console.log('Something went wrong... Try again!');
+  }
 };
 
 const prepareOutputFilename = (filename) => {
@@ -67,8 +75,9 @@ const startApp = async () => {
     options.watermarkText = text.value;
     
     if (fileExists) {
-      console.log('Text Watermark done!');
       addTextWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), options.watermarkText);
+      console.log('Text Watermark done!');
+      startApp();
     } else {
       console.log(`The file ${options.inputImage} dont exist!`);
     };
@@ -82,14 +91,13 @@ const startApp = async () => {
     }]);
     options.watermarkImage = image.filename;
     if (fileExists && fs.existsSync('./img/' + options.watermarkImage)) {
-      console.log('Image Watermark done!');
       addImageWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), './img/' + options.watermarkImage);
+      console.log('Image Watermark done!');
+      startApp();
     } else {
       console.log('Something went wrong... Try again!');
     }
   }
-
-  startApp();
 }
 
 startApp();
